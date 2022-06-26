91e93dce11b025ab213dfb3bd0af10737bfbeb3e
Nickson Santos Endlich <nickson12@gmail.com>
2022-06-26 13:41:34 -0300

    Fim da Aula 1 - Testes - Alura - Testes em .NET: testando software

---
+++ Alura.Estacionamento.Testes/TipoVeiculoTestes.cs
@@ -0,0 +1,33 @@
+﻿using Alura.Estacionamento.Alura.Estacionamento.Modelos;
+using Alura.Estacionamento.Modelos;
+
+namespace Alura.Estacionamento.Testes
+{
+    public class TipoVeiculoTestes
+    {
+        // Arrange
+        // Act
+        // Assert
+
+        [Fact]
+        public void TestaTipoVeiculo()
+        {
+            // Arrange
+            var veiculo = new Veiculo();
+            // Act
+            // Assert
+            Assert.Equal(TipoVeiculo.Automovel, veiculo.Tipo);
+        }
+
+        [Fact]
+        public void TestaTipoVeiculoMotocicleta()
+        {
+            // Arrange
+            var veiculo = new Veiculo();
+            // Act
+            veiculo.Tipo = TipoVeiculo.Motocicleta;
+            // Assert
+            Assert.Equal(TipoVeiculo.Motocicleta, veiculo.Tipo);
+        }
+    }
+}
